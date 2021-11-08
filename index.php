3ed73872869bbf1e6a63858f98bb67c3d4d534da
Mélanie Loubet <melanieloubet29@gmail.com>
2021-11-08 21:58:23 +0100

    cursor is placed at the begining of the new task after adding it

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 			<ul>
 				<li v-for="(task, index) in tasks" :key="index">
 					<i @click="task.done = !task.done" v-bind:class="{ 'far fa-square': !task.done, 'far fa-check-square': task.done }"></i>
-					<span class="task-name" contenteditable="true" @keyup.enter="addTaskAfter($event, index, task)" @keyup.delete="removeTaskBefore($event, index)" @keyup="renameTask($event, index)">{{ task.text }}</span>
+					<span class="task-edit" contenteditable="true" @keyup.enter="addTaskAfter($event, index, task)" @keyup.delete="removeTaskBefore($event, index)" @keyup="renameTask($event, index)">{{ task.text }}</span>
 				</li>
 			</ul>
 		</div>
@@ -42,12 +42,25 @@
 				return caretPos;
 			}
 
-			 function setup() {
-			    // mounted
-			    onUpdated(() => {
-			      console.log('Component is mounted!')
-			  });
-			  }
+			//BUG : pressing suppr while editing doesn't do anything
+
+			function moveCursor(elem, index){
+				if(elem != null && index != null) {
+					if(elem.createTextRange) {
+						var range = elem.createTextRange();
+						range.move('character', caretPos);
+						range.select();
+					}
+					else {
+						if(elem.selectionStart) {
+							elem.focus();
+							elem.setSelectionRange(caretPos, caretPos);
+						}else{
+							elem.focus();
+						}
+					}
+				}
+			}
 
 			const app = Vue.createApp({
 				data() {
@@ -57,24 +70,25 @@
 							{ text: 'Learn Vue', done : true },
 							{ text: 'Build something awesome', done : false}
 						],
-						cursorPos : [null, null]//element and index in which to place cursor
+						cursorPos : {
+							taskIndex : null,
+							charAt : null
+						}
 					}
 				},
+				updated(){
+					var tEl = document.getElementsByClassName('task-edit');
+					moveCursor(tEl[this.cursorPos.taskIndex], this.cursorPos.charAt);
+				},
 				methods : {
-					// onkeydown(evt, index, task){
-					// 	if(evt.keycode == 13){
-					// 		this.addTaskAfter(evt, index);
-					// 	}else if(evt.keycode == 46){
-					// 		this.removeTaskBefore(evt, index);
-					// 	}else{
-					// 		this.renameTask(evt, index);
-					// 	}
-					// },
 					addTaskAfter(evt, index, task){
 						var t = evt.target.innerText.split('\n', 2);
 						this.tasks[index].text = t[0].replace('\n','');
 						evt.target.innerText = t[0].replace('\n','');
 						this.tasks.splice(index + 1, 0, {text : t[1].replace('\n','') , done : false});
+						//set info to place cursor at the begining of new task on next render
+						this.cursorPos.taskIndex = index + 1;
+						this.cursorPos.charAt = 0;
 						// TODO : place cursor at the begining of new task
 					},
 					removeTaskBefore(evt, index){
@@ -87,26 +101,8 @@
 					},
 					renameTask(evt, index){
 						this.tasks[index].text = evt.target.innerText;
-						this.moveCursor(evt.target, evt.target.innerText.length);
+						// this.moveCursor(evt.target, evt.target.innerText.length);
 						console.log(evt.target.innerText);
-					},
-					moveCursor(elem, index){
-						console.log(index);
-						if(elem != null && index != null) {
-							if(elem.createTextRange) {
-								var range = elem.createTextRange();
-								range.move('character', caretPos);
-								range.select();
-							}
-							else {
-								if(elem.selectionStart) {
-									elem.focus();
-									elem.setSelectionRange(caretPos, caretPos);
-								}else{
-									elem.focus();
-								}
-							}
-						}
 					}
 				}
 			});
@@ -116,10 +112,6 @@
 		<style>
 			ul li {
 				list-style: none;
-			}
-			.task-name, i {
-				display: inline-block;
-				min-width: 10
 			}
 		</style>
 	</body>
